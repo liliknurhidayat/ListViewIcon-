24720469e80cd4e6317aa1a9a877ecb0434fd524
liliknurhidayat <44837780+liliknurhidayat@users.noreply.github.com>
2019-10-14 09:05:29 -0600

    Add files via upload

---
+++ Model_flower.kt
@@ -0,0 +1,18 @@
+package com.example.listku
+
+/**
+ * Created by deepshikha on 31/7/17.
+ */
+public class Model_flower{
+    var str_name:String =""
+    var str_des:String =""
+    var int_image:Int=0
+
+    constructor(str_name:String,int_image:Int,str_des:String){
+        this.int_image=int_image
+        this.str_name=str_name
+        this.str_des=str_des
+
+    }
+
+}
